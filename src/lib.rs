367bc96292f33cc31d0bdbae53660323b397b778
matt rice <ratmice@gmail.com>
2024-01-09 05:50:47 -0800

    another pass over docs

--- src/lib.rs
+++ src/lib.rs
@@ -246,22 +246,32 @@ where
             tool,
         }
     }
+    /// 1. Notes the indication of an error for later observation.
+    /// 2. Sends the error off to be owned by `self.diagnostics`.
+    /// 3. Returns a `ToolError::ToolFailure`
     pub fn emit_error(&mut self, e: X::Error) -> Result<(), ToolError> {
         self.observed_error = true;
         self.diagnostics.emit_error(e);
         Err(ToolError::ToolFailure)
     }
+    /// 1. Notes the indication of an error for later observation.
+    /// 2. Sends the error off to be owned by `self.diagnostics`.
     pub fn emit_non_fatal_error(&mut self, e: X::Error) {
         self.observed_error = true;
         self.diagnostics.emit_error(e);
     }
+    /// 1. Notes the indication of the warning for later observation.
+    /// 2. Sends the warning off to be owned by `self.diagnostics`.
     pub fn emit_warning(&mut self, w: X::Warning) {
         self.observed_warning = true;
         self.diagnostics.emit_warning(w);
     }
+
+    /// Returns whether any errors have been observed during it's lifetime.
     pub fn observed_error(&self) -> bool {
         self.observed_error
     }
+    /// Returns whether any warnings have been observed during it's lifetime.
     pub fn observed_warning(&self) -> bool {
         self.observed_warning
     }
@@ -300,17 +310,12 @@ impl<X: Tool> Default for SimpleDiagnostics<X> {
 }
 
 impl<X: Tool> Diagnostics<X> for SimpleDiagnostics<X> {
-    /// Indicatation that an error has occurred and the
-    /// `Diagnostics` should take ownership.
     fn emit_error(&mut self, e: X::Error) {
         self.errors.push(e);
     }
-    /// Indicatation that a `warning` has occurred and the
-    /// `Diagnostics` should take ownership of it.
     fn emit_warning(&mut self, w: X::Warning) {
         self.warnings.push(w);
     }
-    /// Called by the `DiagnosticsEmitter` drop handler.
     fn no_more_data(&mut self) {
         println!("no_more_data");
     }
@@ -320,6 +325,7 @@ impl<'diag, X: Tool, Diag> Drop for DiagnosticsEmitter<'diag, X, Diag>
 where
     Diag: Diagnostics<X>,
 {
+    /// Calls `Diagnostics::no_more_data()`
     fn drop(&mut self) {
         self.diagnostics.no_more_data()
     }
@@ -356,10 +362,20 @@ pub trait Spanned: fmt::Display {
 }
 
 pub trait Diagnostics<X: Tool> {
+    /// Indicatation that an error has occurred and the
+    /// `Diagnostics` should take ownership of it.
     fn emit_error(&mut self, error: X::Error);
+    /// Indicatation that a `warning` has occurred and the
+    /// `Diagnostics` should take ownership of it.
     fn emit_warning(&mut self, warning: X::Warning);
-    /// Is called from `DiagnosticsEmitter::drop` To the top-level
-    /// diagnostics value. Receivers may need to propagate it.
+    /// Called by the `DiagnosticsEmitter` drop handler.
+    /// The receiver may need to propagate this call.
+    ///
+    /// If an implementation buffers errors emitting them here
+    /// they should consider the case where the diagnostics
+    /// outlives multiple `DiagnosticsEmitters`s by being passed
+    /// through multiple drivers, and drain those buffers here.
+    /// To avoid emitting the same errors multiple times.
     fn no_more_data(&mut self);
 }
 
